88156c9ff7383ae7e4cb692004c585b8f07d0197
ashutoshpith <amxdp18@gmail.com>
2020-04-03 23:24:24 +0530

    initial commit

---
+++ src/commands/goodbye.ts
@@ -0,0 +1,7 @@
+import { Command } from "@oclif/command";
+
+export class GoodByeCommand extends Command{
+    async run(){
+        console.log("goodbye");
+    }
+}
---
+++ src/commands/startServer.ts
@@ -0,0 +1,44 @@
+import { Command } from "@oclif/command";
+import * as fastify from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
+
+export class StartServer extends Command{
+    async run(){
+      try {
+        console.log("starting");
+        await this.start();
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+    async start(){
+        const server: fastify.FastifyInstance = fastify({})
+
+        const opts: fastify.RouteShorthandOptions = {
+          schema: {
+            response: {
+              200: {
+                type: 'object',
+                properties: {
+                  pong: { type: 'string'}
+                }
+              }
+            }
+          }
+        }
+        
+        server.get('/', opts, async (request, reply) => {
+          return { pong: 'it worked!' }
+        })
+        
+        server.listen(3000, (err) => {
+          if (err) {
+            server.log.error(err)
+            process.exit(1)
+          }
+          server.log.info(`server listening on ${server.server.address()}`)
+          console.log(`server listening on ${server.server.address().port}`)
+        })
+    }
+}
