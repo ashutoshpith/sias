43a0d60adfa8db46760ffa5604ab5d9417c82b59
ashutoshpith <amxdp18@gmail.com>
2020-04-05 03:04:27 +0530

    notifier added

--- src/commands/file/set.ts
+++ src/commands/file/set.ts
@@ -1,12 +1,16 @@
 import {Command, flags} from "@oclif/command";
 import * as fs from "fs-extra";
 import * as path from "path";
+import * as notifier from "node-notifier";
 
 export class Set extends Command{
 
     
       async run() {
-    
+       notifier.notify({
+           title:"set task",
+           message:"completed"
+       });
         console.log("set")
       }
 }
