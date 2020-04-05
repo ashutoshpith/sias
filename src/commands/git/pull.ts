9744d7e816414ec5af6fe1d53c61bea5b75cf84e
ashutoshpith <amxdp18@gmail.com>
2020-04-05 07:11:26 +0530

    git pull added

--- src/commands/git/pull.ts
+++ src/commands/git/pull.ts
@@ -1,7 +1,27 @@
-import { Command } from "@oclif/command";
-
+import { Command, flags } from "@oclif/command";
+import cli from "cli-ux";
+import { spawn } from "child_process";
 export class Pull extends Command{
-    async run(){
-        console.log("pull")
-    }
+    static flags = {
+        help: flags.help({char: 'h'}),
+      }
+    
+      static args = [{name: 'input'}]
+      
+      async run(){
+         const {flags} = this.parse(Pull);  
+         const ls = spawn(`git pull`,{shell:true});
+         cli.action.start("process started ! ...");
+         ls.stdout.on('data', (data) => {
+          console.log(`stdout recived: ${data}`);
+        });
+        
+        ls.stderr.on('data', (data) => {
+          console.error(`stderr: ${data}`);
+        });
+        
+        ls.on('close', (code) => {
+          console.log(`child process exited with code ${code}`);
+        });
+      }
 }
