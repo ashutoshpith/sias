aa5887379272c9765e5bfa15d188863bc435b793
ashutoshpith <amxdp18@gmail.com>
2020-04-05 06:40:22 +0530

    "git"

--- src/commands/git/commit.ts
+++ src/commands/git/commit.ts
@@ -5,18 +5,17 @@ import * as fs from "fs-extra";
 export class Commit extends Command{
     // static description = 'describe the command here'
   
-    // static flags = {
-    //   help: flags.help({char: 'h'}),
-    //   name: flags.string({char: 'n', description: 'name to print'}),
-    //   force: flags.boolean({char: 'f'}),
-    // }
+    static flags = {
+      help: flags.help({char: 'h'}),
+      message: flags.string({char: 'm', description: 'message to commit'}),
+    }
   
     static args = [{name: 'input'}]
     
     async run(){
-        // const data = execFileSync("../../structs/sh/git-commit.sh");
-
-       const ls = spawn("sudo src/structs/sh/git-commit.sh",{shell:true})
+       const {flags} = this.parse(Commit);            
+       const ls = spawn(`git add . 
+                        git commit -m '"${flags.message}"' `,{shell:true})
        ls.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
       });
