05c0e9afe19cbe40fd3eb7a78d0995745877dceb
ashutoshpith <amxdp18@gmail.com>
2020-04-05 06:51:25 +0530

    -m=git-push-command-added

--- src/commands/git/commit.ts
+++ src/commands/git/commit.ts
@@ -1,21 +1,20 @@
 import { Command, flags } from "@oclif/command";
 import { exec, spawn, fork} from "child_process";
-import * as fs from "fs-extra";
 
 export class Commit extends Command{
     // static description = 'describe the command here'
   
     static flags = {
       help: flags.help({char: 'h'}),
-      message: flags.string({char: 'm', description: 'message to commit'}),
+    //   message: flags.string({char: 'm', description: 'message to commit'}),
     }
   
     static args = [{name: 'input'}]
     
     async run(){
-       const {flags} = this.parse(Commit);            
+       const {argv,flags} = this.parse(Commit);            
        const ls = spawn(`git add . 
-                        git commit -m '"${flags.message}"' `,{shell:true})
+                        git commit -m "${argv[0]}" `,{shell:true})
        ls.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
       });
--- src/commands/git/push.ts
+++ src/commands/git/push.ts
@@ -1,7 +1,19 @@
 import { Command } from "@oclif/command";
+import { exec, spawn, fork} from "child_process";
 
 export class Push extends Command{
     async run(){
-        console.log("push")
+        const ls = spawn(`git push `,{shell:true})
+        ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        });
+
+        ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+        });
+
+        ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        });
     }
 }
