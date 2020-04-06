3db5b4316663f27412650ab70f7d9278ff868c8a
ashutoshpith <amxdp18@gmail.com>
2020-04-07 04:07:20 +0530

    sshkey on initial stage

--- src/commands/ssh/rsa.ts
+++ src/commands/ssh/rsa.ts
@@ -1,13 +1,30 @@
 import { Command, flags } from "@oclif/command";
-
+import { spawn } from "child_process";
 export class Rsa extends Command {
   static flags = {
+    emai: flags.string({
+      char: "e",
+      default: "ashutoshpith@gmail.com",
+    }),
     help: flags.help({
       char: "h",
     }),
   };
   async run() {
     const { flags } = this.parse(Rsa);
-    console.log("ssh key");
+    const ls = spawn(`ssh-keygen -t rsa -b 4096 -C "${flags.emai}"`, {
+      shell: true,
+    });
+    ls.stdout.on("data", (data) => {
+      console.log(`stdout message recived: ${data}`);
+    });
+
+    ls.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    ls.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
   }
 }
